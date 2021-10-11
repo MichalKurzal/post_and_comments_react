c37dce6251f500d4f418601881d9c2072501abba
Michal Kurzal <michalkurzal200@gmail.com>
2021-10-11 21:27:01 +0200

    Modal for new Post form

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,13 +4,26 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
 import { Link } from "react-router-dom";
-
 import { useLocation } from "react-router-dom";
 
 export default function NavBar() {
   const location = useLocation();
-  console.log(location);
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,7 +37,31 @@ export default function NavBar() {
             </Link>
           ) : null}
           {location.pathname !== "/" ? (
-            <Button color="inherit">New Post</Button>
+            <div>
+              <Button onClick={handleOpen} color="inherit">
+                New Post
+              </Button>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    Create a new Post
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    Duis mollis, est non commodo luctus, nisi erat porttitor
+                    ligula.
+                  </Typography>
+                </Box>
+              </Modal>
+            </div>
           ) : null}
           {location.pathname !== "/" ? (
             <Link to="/user">
