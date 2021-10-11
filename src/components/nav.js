923ee2302dfc5cb54303bb23e1db950d9ab7ce04
Michal Kurzal <michalkurzal200@gmail.com>
2021-10-11 20:10:04 +0200

    1.Styling for userPage. 2.Logout button

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,8 +4,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 
 import { useLocation } from "react-router-dom";
@@ -17,18 +15,14 @@ export default function NavBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Coding Case
           </Typography>
+          {location.pathname !== "/" ? (
+            <Link to="/">
+              <Button color="inherit">Logout</Button>
+            </Link>
+          ) : null}
           {location.pathname !== "/" ? (
             <Button color="inherit">New Post</Button>
           ) : null}
--- src/components/userPage.js
+++ src/components/userPage.js
@@ -1,5 +1,59 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+
 const UserPage = () => {
-  return <h2>UserPage</h2>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: 10,
+      }}
+    >
+      <Typography variant="h3">User Name</Typography>
+
+      <Stack
+        direction="row"
+        spacing={3}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h6">Name</Typography>
+        <Typography>Placeholder</Typography>
+      </Stack>
+      <Stack
+        direction="row"
+        spacing={3}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h6">Age</Typography>
+        <Typography>Placeholder</Typography>
+      </Stack>
+      <Stack
+        direction="row"
+        spacing={3}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h6">Adress</Typography>
+        <Typography>Placeholder</Typography>
+      </Stack>
+      <Stack
+        direction="row"
+        spacing={3}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h6">E-Mail</Typography>
+        <Typography>Placeholder</Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default UserPage;
