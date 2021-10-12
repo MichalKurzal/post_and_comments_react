0fd5f013880bc50657cefdb87a68fd30577348da
Michal Kurzal <michalkurzal200@gmail.com>
2021-10-12 22:57:57 +0200

    refactoring

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import { Link } from "react-router-dom";
-import newPost from "../api/postComment";
+import newPost from "../api/newPost";
 import { useLocation } from "react-router-dom";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 
